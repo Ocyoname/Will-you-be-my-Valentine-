7c6b983fdefa31190824f5310254796a17002117
Ocyoname <bayualfalaq33@gmail.com>
2025-02-14 00:41:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -54,16 +54,16 @@
 })();
 */
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Kamu beneran gamau?",
+    "Beneran nih??",
+    "serius?",
+    "Sayang, please...",
+    "Harus mw!!",
+    "Kalo gmw aku sedih bgt",
+    "Sedih bgt bgt bgt",
+    "Sedih beneran nih!!",
+    "AH, YAUDA LAH! TERSERAH!",
+    "Tapi boonk, yakali aku nyerah gitu aja :p"
 ];
 
 let messageIndex = 0;
